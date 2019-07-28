9c8e8fbb08b080136370e718a09b86c87ed1b59d
did1335 <did1335@gmail.com>
2019-07-28 21:31:18 +0800

    Create dictionary.js

---
+++ exercise/practice/javascript/dictionary.js
@@ -0,0 +1,7 @@
+d={apple:"蘋果",book:"書",cake:"蛋糕"}
+console.log(d.cake)
+console.log(d["cake"])
+
+t={dog:{name:"snoopy",age:3},cat:{name:"kitty",age:5}}
+console.log("snoopy的年齡:",t.dog.age)
+console.log(t.cat)
