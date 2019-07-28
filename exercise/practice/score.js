a7c746f45c6e91880bbeb4f4d3dd2497ec10cd6c
did1335 <did1335@gmail.com>
2019-07-28 20:30:43 +0800

    score.js

---
+++ exercise/practice/score.js
@@ -0,0 +1,14 @@
+var score=61
+
+if(score>=60){
+    console.log("及格")
+}
+else if(score<0){
+    console.log("Error")
+}
+else if(score===0){
+    console.log("0分")
+}
+else{
+    console.log("不及格")
+}
